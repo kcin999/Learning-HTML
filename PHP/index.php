ce17688d5b6cc83a97ac6df607ea88a6f6753721
Nicholas Bierman <kcinb02@gmail.com>
2021-09-06 15:38:31 -0400

    more refactor

--- PHP/index.php
+++ PHP/index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>Title</title>
-        <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="../globalStyles.css">
     </head>
     <div id="topBar"></div>
     <body>
@@ -13,16 +13,17 @@
         <p id="resultText"></p>
         <div class="slideshow-container">
             <div class="mySlides">
-                <img src="Images/Smith-Hall.jpeg">
+                <img src="../Images/Smith-Hall.jpeg">
             </div>
             <div class="mySlides">
-                <img src="Images/PowerBI.png">
+                <img src="../Images/PowerBI.png">
             </div>
             <div class="mySlides">
-                <img src="Images/busch.jfif">
+                <img src="../Images/busch.jfif">
             </div>
         </div>
         <a href="../index.html">Click here to go back to the home screen to choose JS or PHP</a>
+
     </body>
         <div id="bottomBar">
         <p class="right_aligned" style="color: white;">Inspired by <a style="color: white;" href="https://thesamdickey.com/">Sam Dickey</a></p>
