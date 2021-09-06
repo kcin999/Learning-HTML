f463be31f5553db93a80c47f04e668a124e9a0ae
Nicholas Bierman <kcinb02@gmail.com>
2021-09-05 20:18:03 -0400

    Trying to set up slide show

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 function clickButton(){
     document.getElementById("resultText").innerHTML = "You clicked the button";
 }
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  console.log("I have " + slides.length + " slides")
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
