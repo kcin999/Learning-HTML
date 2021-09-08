96ce441c05f49055953aa551251eb17627bebd84
Nicholas Bierman <kcinb02@gmail.com>
2021-09-07 21:13:25 -0400

    Sidebar Navigation setup

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 function clickButton(){
     console.log("You clicked the button");
 }
+
+function openSideNav(){
+    document.getElementById("sideNav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeSideNav(){
+    document.getElementById("sideNav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
