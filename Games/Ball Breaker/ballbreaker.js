2060e4a24208ef1c94bd1366015595253409de53
kcin999 <kcinb02@gmail.com>
2021-09-09 20:38:03 -0400

    Working on title screen for Brick Breaker

--- Games/Ball Breaker/ballbreaker.js
+++ Games/Ball Breaker/ballbreaker.js
@@ -1,15 +1,23 @@
+//canvas elements
 var canvas = document.getElementById("ballBreakerCanvas");
 var ctx = canvas.getContext("2d");
+
+//ball elements
 var ballRadius = 10;
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
+//paddle elements
 var paddleHeight = 10;
 var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth)/2;
+
+//event handlers
 var rightPressed = false;
 var leftPressed = false;
+
+//Brick elemnts
 var brickRowCount = 3;
 var brickColumnCount = 3;
 var brickWidth = 75;
@@ -17,9 +25,6 @@ var brickHeight = 20;
 var brickPadding = 10;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
-var score = 0;
-var lives = 3;
-
 var bricks = [];
 for(var c=0; c<brickColumnCount; c++) {
   bricks[c] = [];
@@ -28,9 +33,22 @@ for(var c=0; c<brickColumnCount; c++) {
   }
 }
 
+//Game Varibles
+var score = 0;
+var lives = 3;
+var gamestate = "startMenu"
+
+//menu screen variables
+let startBoxWidth = canvas.width / 4;
+let startBoxHeight = canvas.height / 6;
+let startBoxX = (canvas.width/2) - (startBoxWidth/2)
+let startBoxY = (canvas.height / 2) + (startBoxHeight / 2)
+
+
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 document.addEventListener("mousemove", mouseMoveHandler, false);
+document.addEventListener("click",mouseClicked,false);
 
 function keyDownHandler(e) {
     if(e.key == "Right" || e.key == "ArrowRight") {
@@ -40,7 +58,6 @@ function keyDownHandler(e) {
         leftPressed = true;
     }
 }
-
 function keyUpHandler(e) {
     if(e.key == "Right" || e.key == "ArrowRight") {
         rightPressed = false;
@@ -49,12 +66,28 @@ function keyUpHandler(e) {
         leftPressed = false;
     }
 }
-
 function mouseMoveHandler(e) {
   var relativeX = e.clientX - canvas.offsetLeft;
   if(relativeX > 0 && relativeX < canvas.width) {
     paddleX = relativeX - paddleWidth/2;
   }
+}
+function getMousePosition(e){
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: e.clientX - rect.left,
+    y: e.clientY - rect.top
+  }
+}
+function mouseClicked(e){
+  console.log("Mouse clicked");
+  let mousePos = getMousePosition(e);
+  console.log(mousePos)
+
+  if((mousePos.x >= startBoxX && mousePos.x <= (startBoxX + startBoxWidth)) && (mousePos.y>=startBoxY && mousePos.y <=(startBoxY+startBoxHeight))){
+    gamestate = "playGame"
+  }
+  
 }
 function collisionDetection() {
   for(var c=0; c<brickColumnCount; c++) {
@@ -74,7 +107,6 @@ function collisionDetection() {
     }
   }
 }
-
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI*2);
@@ -116,16 +148,14 @@ function drawLives() {
   ctx.fillStyle = "#0095DD";
   ctx.fillText("Lives: "+lives, canvas.width-65, 20);
 }
-
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+function playGame(){
   drawBricks();
   drawBall();
   drawPaddle();
   drawScore();
   drawLives();
   collisionDetection();
-
+  
   if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
     dx = -dx;
   }
@@ -161,6 +191,42 @@ function draw() {
 
   x += dx;
   y += dy;
+}
+function drawTitle(){
+  titleMessage = "Welcome to Brick Breaker"
+  ctx.fillStyle = "#FFFFFF";
+  ctx.font  = "17px Arial";
+  xposition = (canvas.width/2) - (ctx.measureText(titleMessage).width/2);
+  yposition = (canvas.height/2) - (ctx.measureText(titleMessage).actualBoundingBoxAscent/2);
+  ctx.fillText(titleMessage,xposition,canvas.height/2);
+  ctx.closePath();
+}
+function drawStartButton(){
+  ctx.rect(startBoxX,startBoxY,startBoxWidth,startBoxHeight)
+  ctx.fillStyle = "#FFFFFF";
+  ctx.fill();
+
+  //Start Phrase
+  text = "Start"
+  ctx.font = "12px Arial"
+  ctx.fillStyle = "#000000"
+  xStartText = startBoxX + (startBoxWidth/2 - (ctx.measureText(text).width/2));
+  yStartText = startBoxY + ((startBoxHeight / 2) + (ctx.measureText(text).actualBoundingBoxAscent/2));
+  ctx.fillText(text,xStartText,yStartText)
+
+  ctx.closePath()
+}
+function startMenu(){
+  drawTitle();
+  drawStartButton();
+}
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  if(gamestate == "playGame"){
+    playGame();
+  }else if(gamestate == "startMenu"){
+    startMenu();
+  }
   requestAnimationFrame(draw);
 }
 
