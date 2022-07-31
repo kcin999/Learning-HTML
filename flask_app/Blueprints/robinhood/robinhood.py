6aa2b863ca82ff53da7593fbeedcd91c1fe0b6a1
kcin999 <62564441+kcin999@users.noreply.github.com>
2022-07-31 18:42:04 -0400

    Removed Flask App

--- flask_app/Blueprints/robinhood/robinhood.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint, render_template, request, abort
-from backend.models import User, db
-from flask_login import login_required
-
-robinhood = Blueprint('robinhood',__name__)
